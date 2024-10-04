5793c71fedb98ab7a449c13bcf15e4e5547872b4
Naoki Kitamura <108145660+nkitamuraQC@users.noreply.github.com>
2024-10-04 22:36:30 +0900

    Update lattice_model.py

--- pylat/lattice_model.py
+++ pylat/lattice_model.py
@@ -1,4 +1,22 @@
-
+from pylat.template import *
+import pathlib, os
 
 class LatticeModel_gen:
-   pass
+    def __init__(self, lat_type, ints_dict):
+        self.ints_dict = ints_dict
+        self.lat_type = lat_type
+        self.vmcdry_path = ""
+        self.tmp_dir = pathlib.Path("")
+        self.lattice_dict ={}
+
+    def drive(self):
+        temp = self.lattice_dict[self.lat_type]
+        temp = temp.format(**self.ints_dict)
+        wf = open(str(self.tmp_dir.joinpath("tmp.in")), "w")
+        wf.write(temp)
+        wf.close()
+        os.system(f"{self.vmcdry_path} {
+        
+            
+            
+        
