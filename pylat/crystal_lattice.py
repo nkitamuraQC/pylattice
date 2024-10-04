d7ee160444ceff5bf41438a08292435ae6378751
Naoki Kitamura <108145660+nkitamuraQC@users.noreply.github.com>
2024-10-04 21:17:07 +0900

    Update crystal_lattice.py

--- pylat/crystal_lattice.py
+++ pylat/crystal_lattice.py
@@ -1,1 +1,24 @@
+from ase.spacegroup import crystal
 
+class SpaceGroupSupplement:
+    def __init__(self, xyzfile, lattfile):
+        self.xyzfile = xyzfile
+        self.lattfile = lattfile
+
+        self.xyz = None
+        self.elements = None
+        self.latticevec = None
+        self.lattice_param = None
+
+    def read_file(self):
+        return
+
+    def get_lattice_param(self):
+        return
+
+    def supplement_sp(self, spacegroup):
+        atoms = crystal(self.elements, [self.xyz], 
+                        spacegroup=spacegroup,
+                        cellpar=self.lattice_param)
+        return atoms
+        
