b96bddbbd56ec04e2b4ba9dbaadf1b83b4988392
Naoki Kitamura <108145660+nkitamuraQC@users.noreply.github.com>
2024-10-04 21:53:55 +0900

    Update crystal_lattice.py

--- pylat/crystal_lattice.py
+++ pylat/crystal_lattice.py
@@ -1,7 +1,8 @@
 from ase.spacegroup import crystal
 
 class SpaceGroupSupplement:
-    def __init__(self, xyzfile, lattfile):
+    def __init__(self, ciffile, xyzfile, lattfile):
+        self.ciffile = ciffile
         self.xyzfile = xyzfile
         self.lattfile = lattfile
 
@@ -11,6 +12,12 @@ class SpaceGroupSupplement:
         self.lattice_param = None
 
     def read_file(self):
+        return
+
+    def get_frac(self):
+        retutn
+
+    def get_xyz(self):
         return
 
     def get_lattice_param(self):
