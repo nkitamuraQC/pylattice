95f8be404f63f14a185f3ff80c2051eb95b28f1c
Naoki Kitamura <108145660+nkitamuraQC@users.noreply.github.com>
2024-10-08 22:18:27 +0900

    Update template.py

--- pylat/template.py
+++ pylat/template.py
@@ -1,3 +1,78 @@
 
-chain = ""
-triangular = ""
+chain = """
+W={W}
+L={L}
+model = "Fermion Hubbard"
+lattice = "chain lattice" 
+t = {t1}
+t' = {t2}
+U = {U}
+V = {V1} 
+V'= {V2}
+ncond = {nelec}
+"""
+
+triangular = """
+W={W}
+L={L}
+model = "Fermion Hubbard"
+lattice = "triangular lattice" 
+t = {t1}
+t' = {t2}
+U = {U}
+V = {V1} 
+V'= {V2}
+ncond = {nelec}
+"""
+
+square = """
+W={W}
+L={L}
+model = "Fermion Hubbard"
+lattice = "square lattice" 
+t = {t1}
+t' = {t2}
+U = {U}
+V = {V1} 
+V'= {V2}
+ncond = {nelec}
+"""
+
+honeycomb = """
+W={W}
+L={L}
+model = "Fermion Hubbard"
+lattice = "honeycomb lattice" 
+t = {t1}
+t' = {t2}
+U = {U}
+V = {V1} 
+V'= {V2}
+ncond = {nelec}
+"""
+
+kagome = """
+W={W}
+L={L}
+model = "Fermion Hubbard"
+lattice = "kagome lattice" 
+t = {t1}
+t' = {t2}
+U = {U}
+V = {V1} 
+V'= {V2}
+ncond = {nelec}
+"""
+
+ladder = """
+W={W}
+L={L}
+model = "Fermion Hubbard"
+lattice = "ladder lattice" 
+t = {t1}
+t' = {t2}
+U = {U}
+V = {V1} 
+V'= {V2}
+ncond = {nelec}
+"""
