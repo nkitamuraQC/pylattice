886f52bde6dc100117e441c2bec0f896fc14ac11
Naoki Kitamura <108145660+nkitamuraQC@users.noreply.github.com>
2024-10-08 22:23:08 +0900

    Update lattice_model.py

--- pylat/lattice_model.py
+++ pylat/lattice_model.py
@@ -3,14 +3,18 @@ import pathlib, os
 import numpy as np
 
 class LatticeModel_gen:
-    def __init__(self, lat_type, ints_dict):
+    def __init__(self, lat_type, ints_dict, H, W, nelec):
         self.ints_dict = ints_dict
+        self.size_dict = {"H": H, "W": W, "ncond": nelec}
         self.lat_type = lat_type
         self.vmcdry_path = ""
         self.tmp_dir = pathlib.Path("")
-        self.lattice_dict = {"chain": chain}
+        self.lattice_dict = {"chain": chain, "triangular": triangular,
+                             "square": square, "honeycomb": honeycomb,
+                             "ladder": ladder, "kagome": kagome}
 
     def drive(self):
+        self.ints_dict.update(self.size_dict)
         temp = self.lattice_dict[self.lat_type]
         temp = temp.format(**self.ints_dict)
         name = str(self.tmp_dir.joinpath("tmp.in"))
