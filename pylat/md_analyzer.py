a063e02770d52e542bd0e7e10987e62286dfd3a2
Naoki Kitamura <qclove00@NaokinoMacBook-Pro.local>
2024-12-29 18:07:12 +0900

    md計算

---
+++ pylat/md_analyzer.py
@@ -0,0 +1,41 @@
+
+class MD_Analyzer:
+    def __init__(self, prefix):
+        self.prefix = prefix
+        self.input_name = self.prefix + ".in"
+        self.log_name = self.prefix + ".out"
+        self.energies = None
+        self.stress = None
+        self.kinetic = None
+        self.temp = None
+        self.force = None
+
+
+    def get_energies(self):
+        return
+    
+
+    def get_stress_tensors(self):
+        return
+    
+
+    def get_kinetic_energies(self):
+        return
+    
+
+    def get_temperatures(self):
+        return
+    
+
+    def get_forces(self):
+        return
+    
+
+    def get_young(self):
+        return
+    
+
+
+    
+
+
---
+++ pylat/md_properties.py
@@ -0,0 +1,14 @@
+
+class MD_Properties:
+    def __init__(self, md_analyzer):
+        self.md_analyzer = md_analyzer
+    
+
+    def get_young(self):
+        return
+    
+
+
+    
+
+
