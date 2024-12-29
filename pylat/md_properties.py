e6f93440822e73711567ae60e933e5da119fe5d9
Naoki Kitamura <qclove00@NaokinoMacBook-Pro.local>
2024-12-29 19:59:42 +0900

    vc-md追加

--- pylat/md_properties.py
+++ pylat/md_properties.py
@@ -1,8 +1,12 @@
 
 class MD_Properties:
-    def __init__(self, md_analyzer):
-        self.md_analyzer = md_analyzer
+    def __init__(self, md_analyzers_list):
+        self.md_analyzers = md_analyzers_list
     
+    
+    def gather_data(self):
+        return
+
 
     def get_young(self):
         return
